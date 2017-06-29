2d034a3eae88eedb387fbd318f8956397f132ed5
Flavio Castelli <fcastelli@suse.com>
2017-06-29 14:59:55 +0200

    Do not generate autoyast proxy section
    This is no longer needed given we have a script taking care of setting
    this up.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- spec/views/dashboard/autoyast.xml.slim_spec.rb
+++ spec/views/dashboard/autoyast.xml.slim_spec.rb
@@ -3,23 +3,8 @@ require "rails_helper"
 
 describe "dashboard/autoyast" do
   context "when proxy is enabled" do
-    let(:http_proxy) { "squid.corp.net:3128" }
-    let(:https_proxy) { "squid.corp.net:3443" }
-    let(:no_proxy) { "localhost" }
-
     before do
-      assign(:proxy_http, http_proxy)
-      assign(:proxy_https, https_proxy)
-      assign(:proxy_no_proxy, no_proxy)
       assign(:proxy_systemwide, true)
-    end
-
-    it "generates a proxy section" do
-      render
-
-      check_xml_text_node("profile/networking/proxy/http_proxy", http_proxy)
-      check_xml_text_node("profile/networking/proxy/https_proxy", https_proxy)
-      check_xml_text_node("profile/networking/proxy/no_proxy", no_proxy)
     end
 
     it "generates a post partitioning script" do
@@ -42,17 +27,6 @@ describe "dashboard/autoyast" do
       expect(matches.find { |m| m.to_s.include?("set_proxy.sh") }).to be_nil
     end
 
-    it "does not generate a proxy section" do
-      render
-
-      assert_select("profile/networking/proxy", false)
-    end
-
-  end
-
-  def check_xml_text_node(xpath, value)
-    section = assert_select xpath
-    expect(section.children.text).to eq(value)
   end
 
 end
